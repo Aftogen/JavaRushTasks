af185135fbdce2d15b80af61cc7b9a0c3294914d
Aftogen <prudart@ya.ru>
2017-12-22 01:02:28 +0300

    some fixes=)

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -1,13 +1,143 @@
 package com.javarush.task.task20.task2028;
 
-import java.util.List;
+import java.io.Serializable;
+import java.util.*;
+
 
 /* 
 Построй дерево(1)
 */
-public class CustomTree {
+public class CustomTree<E> extends AbstractList<String> implements Cloneable, Serializable {
+    Entry <String> root = new Entry<String>("0");
+    static int N;
     public static void main(String[] args) {
+        List<String> list = new CustomTree();
+        for (int i = 1; i < 16; i++) {
+            list.add(String.valueOf(i));
+        }
+        //System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
+        list.remove("5");
+        //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
+    }
+
+    static class Entry<T> implements Serializable {
+        String elementName;
+        int lineNumber;
+        boolean availableToAddLeftChildren, availableToAddRightChildren;
+        Entry<T> parent, leftChild, rightChild;
+
+        public Entry(String elementName) {
+            this.elementName = elementName;
+            availableToAddLeftChildren=true;
+            availableToAddRightChildren=true;
+        }
+        public void checkChildren(){
+            if (leftChild!=null) availableToAddLeftChildren=false;
+            if (rightChild!=null) availableToAddRightChildren=false;
+        }
+        public boolean isAvailableToAddChildren(){
+            return availableToAddLeftChildren||availableToAddRightChildren;
+        }
+        public void remove(Object o){
+            lineNumber--;
+
+        }
+
 
 
     }
+    public int size(){
+        return N;
+    }
+
+    @Override
+    public boolean add(String s) {
+        Queue<Entry<String>> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            Entry<String> currentNode = queue.poll();
+            currentNode.checkChildren();
+            if (currentNode.isAvailableToAddChildren()) {
+                if (currentNode.availableToAddLeftChildren){
+                    currentNode.leftChild = new Entry<>(s);
+                    currentNode.leftChild.parent = currentNode;
+                    return true;
+                } else if (currentNode.availableToAddRightChildren) {
+                    currentNode.rightChild = new Entry<>(s);
+                    currentNode.rightChild.parent = currentNode;
+                    return true;
+                }
+            } else {
+                if (currentNode.leftChild != null){
+                    queue.offer(currentNode.leftChild);
+                }
+                if (currentNode.rightChild != null){
+                    queue.offer(currentNode.rightChild);
+                }
+            }
+        }
+        return false;
+    }
+
+
+
+
+    @Override
+    public String get(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String set(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void add(int index, String element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String remove(int index) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public List<String> subList(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    protected void removeRange(int fromIndex, int toIndex) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends String> c) {
+        throw new UnsupportedOperationException();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*public String get(int index)
+public String set(int index, String element)
+public void add(int index, String element)
+public String remove(int index)
+public List<String> subList(int fromIndex, int toIndex)
+protected void removeRange(int fromIndex, int toIndex)
+public boolean addAll(int index, Collection<? extends String> c)*/
