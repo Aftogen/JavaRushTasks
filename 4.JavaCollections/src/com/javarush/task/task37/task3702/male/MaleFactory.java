1b6ab22014ab8c1d7e5df18494daa4e1ca4224d0
Aftogen <prudart@ya.ru>
2017-12-24 00:38:49 +0300

    task37.task3702
    Factoru method, Abstract factory

---
+++ 4.JavaCollections/src/com/javarush/task/task37/task3702/male/MaleFactory.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task37.task3702.male;
+
+
+import com.javarush.task.task37.task3702.AbstractFactory;
+import com.javarush.task.task37.task3702.Human;
+
+public class MaleFactory implements AbstractFactory {
+    public Human getPerson(int age){
+        Human human = null;
+        if (age<=KidBoy.MAX_AGE ) {
+            human = new KidBoy();
+        }
+        if (age>KidBoy.MAX_AGE && age<=TeenBoy.MAX_AGE ){
+            human = new TeenBoy();
+        }
+
+        if (age>TeenBoy.MAX_AGE ){
+            human = new Man();
+        }
+        return human;
+    }
+}
