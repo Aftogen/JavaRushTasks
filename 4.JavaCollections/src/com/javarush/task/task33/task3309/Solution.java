e82e314ee6df683945800033dbb7436ac069a1a4
Aftogen <prudart@ya.ru>
2017-12-23 21:30:53 +0300

    task33.task3309
    Archive

--- 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -1,12 +1,21 @@
 package com.javarush.task.task33.task3309;
 
-/* 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import java.io.StringWriter;
+
+/*
 Комментарий внутри xml
 Реализовать метод toXmlWithComment, который должен возвращать строку - xml представление объекта obj.
 В строке перед каждым тегом tagName должен быть вставлен комментарий comment.
 Сериализация obj в xml может содержать CDATA с искомым тегом. Перед ним вставлять комментарий не нужно.
+
 Пример вызова:
 toXmlWithComment(firstSecondObject, "second", "it's a comment")
+
 Пример результата:
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <first>
@@ -19,6 +28,8 @@ toXmlWithComment(firstSecondObject, "second", "it's a comment")
 <!--it's a comment-->
 <second/>
 </first>
+
+
 Требования:
 1. Количество комментариев вставленных в xml должно быть равно количеству тегов tagName.
 2. Метод toXmlWithComment должен возвращать xml в виде строки преобразованной в соответствии с условием задачи.
@@ -27,11 +38,30 @@ toXmlWithComment(firstSecondObject, "second", "it's a comment")
 5. Метод toXmlWithComment должен быть публичным.
 */
 public class Solution {
-    public static String toXmlWithComment(Object obj, String tagName, String comment) {
-        return null;
+    public static String toXmlWithComment(Object obj, String tagName, String comment)  {
+        //Marsh to xml
+        StringWriter writer = new StringWriter();
+        try {
+            JAXBContext context = JAXBContext.newInstance(obj.getClass());
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.marshal(obj, writer);
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+        //Get xmlData
+        String xmlData = writer.toString();
+        String result=null;
+        //Add comments
+        if (xmlData.contains(tagName))
+            result = xmlData.replace("<" + tagName + ">", "<!--" + comment + "-->\n" + "<" + tagName + ">");
+        else result = xmlData;
+        return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
     }
+
+
 }
