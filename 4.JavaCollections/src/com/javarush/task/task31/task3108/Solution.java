534eb24ea390380cd60c8bce307bbe96bb743d04
Aftogen <prudart@ya.ru>
2017-12-26 17:47:04 +0300

    task36.task3611

--- 4.JavaCollections/src/com/javarush/task/task31/task3108/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3108/Solution.java
@@ -6,17 +6,6 @@ import java.nio.file.Paths;
 
 /* 
 Исследуем Path
-Почитай про все методы класса Path.
-Найди такой, который создает относительный путь между текущим и переданным путем.
-Реализуй логику метода getDiffBetweenTwoPaths, он должен возвращать относительный путь.
-Метод main не участвует в тестировании.
-
-
-Требования:
-1. Класс Solution должен содержать метод Path getDiffBetweenTwoPaths(Path path1, Path path2).
-2. Метод Path getDiffBetweenTwoPaths(Path path1, Path path2) должен быть статическим.
-3. Метод getDiffBetweenTwoPaths должен возвращать относительный путь между первым аргументом и вторым.
-4. Для реализации метода getDiffBetweenTwoPaths используй правильный метод объекта Path.
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
